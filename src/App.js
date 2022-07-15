eb0fbf07242c28dd0f3dd88a7d5b4bd9b871291a
JoaoVitorLima242 <jlima0500@gmail.com>
2022-07-14 22:35:28 -0300

    add firebase and update cartItems in db

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import "./App.css";
-import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector(state => state.cart)
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-    
+  
+  useEffect(() => {
+    fetch('https://redux-68018-default-rtdb.firebaseio.com/cartItems.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  },[cart])
+
   return (
     <div className="App">
       {!isLoggedIn ? 
