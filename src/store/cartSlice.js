7934c9027c644175069520ed79a964963d91722c
JoaoVitorLima242 <jlima0500@gmail.com>
2022-07-14 22:10:41 -0300

    remove item and total cart

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -28,7 +28,21 @@ const cartSlice = createSlice({
             }
             state.totalQuantity++
         },
-        removeFromCart(state) {},
+        removeFromCart(state, action) {
+            const id = action.payload;
+
+            const existingItem = state.itemsList.find(item => item.id === id);
+
+            if (existingItem.quantity === 1) {
+                state.itemsList = state.itemsList.filter(item => item.id !== id);
+                
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice -= existingItem.price;
+            }
+
+            state.totalQuantity--;
+        },
         setShowCart(state) {
             state.showCart = !state.showCart
         }
